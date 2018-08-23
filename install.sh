4b99657c83138ec15352dabfef72aa5536334b2d
munHunger <marcusmunger@hotmail.com>
2018-08-23 17:50:48 +0200

    initial setup

---
+++ install.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+ip link set wlp107s0 down
+wifi-menu
+pacman -Syu
+pacman -Syy
+pacman -S xorg-server xorg-apps xorg-xinit xorg-twm xterm --noconfirm --needed
+pacman -S xf86-video-nouveau --noconfirm --needed
+pacman -S sddm --noconfirm --needed
+systemctl enable sddm
+file /etc/systemd/system/display-manager.service
+pacman -S i3-wm --noconfirm --needed
