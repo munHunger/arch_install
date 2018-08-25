167fadf835118c373e40a30c2f38ab164781f42f
munHunger <marcusmunger@hotmail.com>
2018-08-25 09:18:23 +0200

    add user to sudoers

---
+++ install.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+useradd -m munhunger
+echo "Set password for user"
+passwd munhunger
+echo "munhunger ALL=(ALL:ALL) ALL"  >> /etc/sudoers
+pacman -Syu
+pacman -Syy
+pacman -S emacs --noconfirm --needed
+for INSTALL_FILE in $(ls install)
+do
+    /bin/bash install/$INSTALL_FILE
+done
