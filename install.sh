c7cd4b93c5643cc6f28df3c0701fe27a158da060
munHunger <marcusmunger@hotmail.com>
2018-08-27 22:51:25 +0200

    mixing

--- install.sh
+++ install.sh
@@ -4,6 +4,10 @@ chown -R munhunger:munhunger /home/munhunger
 echo "Set password for user"
 passwd munhunger
 echo "munhunger ALL=(ALL:ALL) ALL"  >> /etc/sudoers
+cd /tmp
+git clone https://github.com/munhunger/arch_install.git
+chown -R munhunger:munhunger arch_install
+cd arch_install
 pacman -Syu
 pacman -Syy
 for INSTALL_FILE in $(ls install)
